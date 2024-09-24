f38003c57e83321d10ad3402cc4d7a05e1ca965a
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-24 15:30:14 +0300

    updated: retrofit library - delete (data deleting) process

--- app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsretrofitlibrary/dao/PersonsDaoInterface.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsretrofitlibrary/dao/PersonsDaoInterface.kt
@@ -1,6 +1,16 @@
 package com.ismailmesutmujde.kotlininternetbasedtransactionsretrofitlibrary.dao
 
+import com.ismailmesutmujde.kotlininternetbasedtransactionsretrofitlibrary.datamodel.CRUDAnswer
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface PersonsDaoInterface {
+
+    @POST("kisiler/delete_kisiler.php")
+    @FormUrlEncoded
+    fun deletePerson(@Field("kisi_id") person_id:Int) : Call<CRUDAnswer>
 
 
 }
--- app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsretrofitlibrary/view/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsretrofitlibrary/view/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.ismailmesutmujde.kotlininternetbasedtransactionsretrofitlibrary.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.ismailmesutmujde.kotlininternetbasedtransactionsretrofitlibrary.R
+import com.ismailmesutmujde.kotlininternetbasedtransactionsretrofitlibrary.datamodel.CRUDAnswer
+import com.ismailmesutmujde.kotlininternetbasedtransactionsretrofitlibrary.service.ApiUtils
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        deletePerson()
+    }
+
+    fun deletePerson() {
+        val pdi = ApiUtils.getPersonsDaoInterface()
+
+        pdi.deletePerson(17721).enqueue(object : Callback<CRUDAnswer> {
+            override fun onResponse(call: Call<CRUDAnswer>?, response: Response<CRUDAnswer>?) {
+                if(response != null) {
+                    Log.e("Success", response.body().success.toString())
+                    Log.e("Message", response.body().message)
+                }
+            }
+
+            override fun onFailure(call: Call<CRUDAnswer>?, t: Throwable?) {
+
+            }
+        })
     }
 }
